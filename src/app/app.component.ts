d417d95d8e7a349c418a41a304bc1e02b3eb70b8
Azat Seyrek <azat.seyrek@adesso.com.tr>
2024-08-27 00:52:24 +0300

    using signals in service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { IInvestmentInput } from './models/investment-input.model';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
-import { IResult } from './models/result.model';
 
 @Component({
   selector: 'app-root',
@@ -11,30 +9,4 @@ import { IResult } from './models/result.model';
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
 })
-export class AppComponent {
-  resultsData = signal<IResult[] | undefined>(undefined);
-
-  onCalculateInvestmentResults(data: IInvestmentInput) {
-    const { initialInvestment, annualInvestment, expectedReturn, duration } = data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-    // this.resultsData = annualData;
-
-    this.resultsData.set(annualData);
-  }
-}
+export class AppComponent {}
--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,6 +1,6 @@
-import { Component, input } from '@angular/core';
-import { IResult } from '../models/result.model';
+import { Component, computed, inject } from '@angular/core';
 import { CurrencyPipe, NgForOf, NgIf } from '@angular/common';
+import { InvestmentService } from '../investment.service';
 
 @Component({
   selector: 'app-investment-results',
@@ -10,5 +10,12 @@ import { CurrencyPipe, NgForOf, NgIf } from '@angular/common';
   styleUrl: './investment-results.component.css',
 })
 export class InvestmentResultsComponent {
-  results = input<IResult[]>();
+  // constructor(private InvestmentService: InvestmentService) {} // or
+  private InvestmentService = inject(InvestmentService);
+
+  // get results() {
+  //   return this.InvestmentService.resultData;
+  // } or we can use computed property. Using computed property is better because it will recompute the value only when the dependencies change.
+
+  results = computed(() => this.InvestmentService.resultData());
 }
